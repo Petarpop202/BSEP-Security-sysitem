357adf8e8a364aab95e5f56cbcc69b49eb041243
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2023-04-05 14:40:53 +0200

    Backend initialized

---
+++ PKI/PKI-Backend/PKI-Security/src/test/java/com/example/PKISecurity/PkiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PKISecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
