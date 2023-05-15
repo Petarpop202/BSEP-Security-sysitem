c01cb70b08f4d7d773f92853d92dc58566c3e412
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2023-05-15 18:23:15 +0200

    Init main apk BE

---
+++ Main Apk/Main Apk-BACKEND/mainAPK/src/main/java/com/example/mainAPK/MainApkApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainAPK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApkApplication.class, args);
+	}
+
+}
